5bb411aa0431027b5923de24474baf014921b9e8
ellsonmendesyt <elsonmendesoficial@gmail.com>
2021-06-04 14:17:12 -0300

    pus jwt nas rotas, inserir token na aplicacao

--- api.js
+++ api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors    = require('cors');
 const {sequelize,Usuario, Carrosel,Produto}  = require('./models');
+const { restart } = require('nodemon');
+const jwt= require('jsonwebtoken')
+
 
 const app = express();
 app.use(express.json())
@@ -10,7 +13,8 @@ app.get('/',(req,res)=>{
     res.status(200).send({api:'sucesso'});
 })
 
-
+//guardar senha
+const jwtSalt='euchoquemeutecladoestáquebradohahaha';
 
 
 ///CRIAR UM USUARIO
@@ -35,20 +39,18 @@ app.get('/usuarios', async (req,res)=>{
         return res.status(200).json(usuarios);
     }catch(erro){
         return res.status(500).json({msg:'erro'})
-        console.error(erro);
     }
 })
 
 
 ///BUSCAR USUARIO PELO ID
-app.get('/usuario/:id', async(req,res)=>{
+app.get('/usuario/:id',auth, async(req,res)=>{
     const {id} = req.params;
     try{
         const usuario = await Usuario.findOne({where:{id}})
         res.status(200).json(usuario);
     }catch(erro){
         return res.status(401).json({'msg':'não conseguimos encontrar o dado buscado'})
-        console.error(erro);
     }
 })
 
@@ -136,3 +138,75 @@ app.listen(4000,async()=>{
 
     console.log('conectado com o banco');
 })
+
+
+app.post('/auth', async(req,res)=>{
+    const {nome,email,senha}=req.body;
+
+    if(email == undefined){
+      res.status(400).json({mgs:'email vazio'})
+    }else{
+
+        try{
+            const usuario = await Usuario.findOne({where:{email}})
+
+             if(usuario !==undefined){
+                if(usuario.senha ==senha){
+
+                    ///cria o payload do toke
+                    jwt.sign({nome:usuario.nome, email:usuario.email},jwtSalt,{expiresIn:'48h'},(err,token)=>{
+                        if(err){
+                            res.status(400).json({msg:'falha interna'})
+                        }else{ 
+                            res.status(200).json({token:token})
+                        }
+                    })
+
+                }else{
+                    res.status(401).json({msg:'credenciais invalidas'});
+                }
+            }else{
+                 res.status(404).json({msg: 'usuario nao encontrado'})
+             }
+
+            // res.status(200).json(usuario);
+        }catch(erro){
+             res.status(401).json({'msg':'email ou senha incorreto'})
+           
+        }
+
+    }
+  })
+
+
+
+  function auth(req,res,next){
+
+    const authToken = req.headers['authorization'];
+    console.log(authToken)
+
+    if(authToken!== undefined){
+        const bearer = authToken.split(' ');
+        const token= bearer[1];
+
+        jwt.verify(token,jwtSalt, (erro,dados)=>{
+            if(erro){
+                res.status(401).json({erro:'token invalido'})
+
+                
+            }else{
+                //se ta tudo ok, cria uma compo na req
+                req.token=token;
+                req.usuarioLogado= {id:dados.id, email:dados.email}
+                next(); //so prosegue se validou
+                console.log(dados);
+            }
+        })
+
+
+    }else{
+        //vazio
+        res.status(401).json({erro: 'Token invalido'})
+    }
+    // next();
+  }
